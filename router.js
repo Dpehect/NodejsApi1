7f8f0af63ab894e54b750abeb9502d5da9e8f9ea
Dpehect <105203804+Dpehect@users.noreply.github.com>
2022-10-13 17:55:52 +0300

    Update router.js

--- router.js
+++ router.js
@@ -7,11 +7,7 @@ const {
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Let's build a CRUD API!");
-});
-
-router.get("/todos", getTodos);
+router.get("/", getTodos);
 router.post("/todos", createTodo);
 router.put("/todos/:todoID", updateTodo);
 router.delete("/todos/:todoID", deleteTodo);
